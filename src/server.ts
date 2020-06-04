7872dc3ff48786ef3d1c60f13c7e6be795ef1a9a
bdsqqq <igorbedesqui@gmail.com>
2020-06-04 03:18:06 -0300

    implemented routes and controllers for server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,14 @@
-import express, { response } from 'express';
+import express from 'express';
+import path from 'path';
+import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-    console.log('Listagem de usuários');
+app.use(cors())
+app.use(express.json());
+app.use(routes);
 
-    res.json([
-        'Diego',
-        'Cleiton',
-        'Robson',
-        'Daniel'
-    ]);
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..','uploads')));
 
 app.listen(3333);
-
