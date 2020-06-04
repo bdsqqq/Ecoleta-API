3fe7a7ec9abd96b3173192efc0badb2b3d3725cc
bdsqqq <igorbedesqui@gmail.com>
2020-06-03 22:47:01 -0300

    Implemented basic express functionality

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express, { response } from 'express';
+
+const app = express();
+
+app.get('/users', (req, res) => {
+    console.log('Listagem de usuários');
+
+    res.json([
+        'Diego',
+        'Cleiton',
+        'Robson',
+        'Daniel'
+    ]);
+});
+
+app.listen(3333);
+
